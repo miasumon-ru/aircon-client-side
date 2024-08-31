4eeae3f8a56612522a1043e8ae684c7eaf404b37
miasumon <miasumon.ru@gmail.com>
2024-08-31 12:40:34 +0600

    z index in the navbar for mobile screen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-96 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[50] mt-3 w-96 p-2 shadow">
 
                             {navLinks}
 
